18ee32c5647f592ee94de8c29e0b21ba94ee5504
Thiago Mohallem <thiago.mohallem@toroinvestimentos.com.br>
2022-11-09 11:54:11 -0300

    teste

--- android/src/main/kotlin/com/example/sfmc_plugin/MainApplication.kt
+++ android/src/main/kotlin/com/example/sfmc_plugin/MainApplication.kt
@@ -18,30 +18,31 @@ class MainApplication : BaseApplication() {
             setMarketingCloudServerUrl(BuildConfig.MC_SERVER_URL)
             setDelayRegistrationUntilContactKeyIsSet(true)
             setUrlHandler(this@MainApplication)
-            setNotificationCustomizationOptions(  
-                    NotificationCustomizationOptions.create(R.drawable.ic_notification,
-                    NotificationManager.NotificationLaunchIntentProvider 
-                    { context, notificationMessage ->
-                        val requestCode = Random().nextInt()
-                        val url = notificationMessage.url
-                        when {
-                            url.isNullOrEmpty() ->
-                            PendingIntent.getActivity(
-                                context,
-                                requestCode,
-                                Intent(context, MainActivity.class),
-                                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-                            )
-                            else ->
-                            PendingIntent.getActivity(
-                                context,
-                                requestCode,
-                                Intent(Intent.ACTION_VIEW, Uri.parse(url)),
-                                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-                            )
-                        }
+            setNotificationCustomizationOptions(
+                NotificationCustomizationOptions.create(R.drawable.ic_notification,
+                    NotificationManager.NotificationLaunchIntentProvider { context, notificationMessage ->
+                    val requestCode = Random().nextInt()
+                    val url = notificationMessage.url
+                    when {
+                        url.isNullOrEmpty() ->
+                        PendingIntent.getActivity(
+                            context,
+                            requestCode,
+                            Intent(context, MainActivity.class),
+                            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                        )
+                        else ->
+                        PendingIntent.getActivity(
+                            context,
+                            requestCode,
+                            Intent(Intent.ACTION_VIEW, Uri.parse(url)),
+                            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                        )
                     }
-                    )  
+                    }
                 )
+            )
         }
 }
+
+
