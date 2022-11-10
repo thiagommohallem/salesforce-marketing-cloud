f4ee168627e3eef294ca6821b1039b15ac419187
Thiago Mohallem <thiago.mohallem@toroinvestimentos.com.br>
2022-11-10 15:35:33 -0300

    teste

--- android/src/main/kotlin/com/example/sfmc_plugin/MainApplication.kt
+++ android/src/main/kotlin/com/example/sfmc_plugin/MainApplication.kt
@@ -21,7 +21,17 @@ class MainApplication : BaseApplication() {
             setDelayRegistrationUntilContactKeyIsSet(true)
             setUrlHandler(this@MainApplication)
             setNotificationCustomizationOptions(
-                NotificationCustomizationOptions.create(R.drawable.ic_notification)
+                NotificationCustomizationOptions.create(R.drawable.ic_notification),
+                NotificationManager.NotificationLaunchIntentProvider { context, notificationMessage ->
+                    val requestCode = Random().nextInt()
+                    val url = notificationMessage.url
+                    PendingIntent.getActivity(
+                            context,
+                            requestCode,
+                            Intent(Intent.ACTION_VIEW, Uri.parse(url)),
+                            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                        )
+                    }, null
             )
         }
 }
